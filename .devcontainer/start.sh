140b0b537a0a1999c917b1b1e0834d372820d5ca
Nisan Ohana <78907315+nisanohana3@users.noreply.github.com>
2023-04-11 15:25:07 +0000

    inital commit

---
+++ .devcontainer/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f custom/conf/app.ini ]
+then
+    mkdir -p custom/conf/
+    echo -e "[server]\nROOT_URL=$(gp url 3000)/" > custom/conf/app.ini
+    echo -e "\n[database]\nDB_TYPE = sqlite3\nPATH = $GITPOD_REPO_ROOT/data/gitea.db" >> custom/conf/app.ini
+fi
+export TAGS="sqlite sqlite_unlock_notify"
+
+make watch-backend & make watch-frontend
